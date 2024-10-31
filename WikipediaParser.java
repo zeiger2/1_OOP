223d19f91249959544d74c72c0ad729b49434051
zeiger2 <zverevart@inbox.ru>
2024-10-31 14:38:02 +0300

    pff
    Словарь - неудачная затея

--- WikipediaParser.java
+++ WikipediaParser.java
@@ -10,6 +10,8 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class WikipediaParser {
@@ -39,18 +41,22 @@ public class WikipediaParser {
 //        catch (ArithmeticException e){
 //            System.out.println("Статьи закончились");
 //        }
-
+        Map<String,String> dictionary = new HashMap<String,String>();
+        int len=0;
         try {
             JsonArray searchResults = root.getAsJsonObject("query").getAsJsonArray("search");
+            len=searchResults.size();
             for (int i = 0; i < searchResults.size(); i++) {
                 String pageTitle = searchResults.get(i).getAsJsonObject().get("title").getAsString();
                 String pageId = searchResults.get(i).getAsJsonObject().get("pageid").getAsString();
-                System.out.println(pageTitle);
-                System.out.println(pageId);
+//                System.out.println(pageTitle);
+//                System.out.println(pageId);
+                dictionary.put(pageTitle, pageId);
             }
         } catch (Exception e){
             System.out.println("Ошибка: Больше статей нет");
         }
+        System.out.println(dictionary.keySet());
 
 //        URI pageUri = new URI("https://ru.wikipedia.org/w/index.php?curid=" + pageId);
 //        java.awt.Desktop.getDesktop().browse(pageUri);
